da68171f83b631ae4b2aed902b82d71360638eea
JItesh Gangwani <jiteshgangwani@jiteshs-air.lan>
2022-09-20 10:20:23 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # trade-service
 
 
-Run microservice
+Run microservice from command prompt
 
-java -jar 
+mvn install ;
+java  -jar target/trade-service-0.0.1-SNAPSHOT.jar
+
 
 curl command to test
 ====================
+
 curl -v  --request POST   --data-binary "@trade.csv" --header 'Content-Type: text/csv' --header 'Accept: text/csv' http://localhost:8081/api/v1/enrich
 
+--data-binary have to be used to maintain formatting of csv file as part of request
 
 
 TODO
--- src/main/java/com/tradedemo/application/service/ProductService.java
+++ src/main/java/com/tradedemo/application/service/ProductService.java
@@ -8,13 +8,14 @@ import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import com.tradedemo.application.model.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,22 +25,26 @@ public class ProductService {
     private static final Logger logger = LoggerFactory.getLogger(ProductService.class);
     Map<String, Product> products = new HashMap<>();
 
+    @Value(value = "classpath:companies.xml")
+    private Resource product;
 
     public ProductService() {
 
         try {
-            File file = new ClassPathResource("product.csv").getFile();
+
+            Resource resource = new ClassPathResource("product.csv");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+
             CsvMapper csvMapper = new CsvMapper();
             CsvSchema schema = CsvSchema.emptySchema().withHeader().withColumnSeparator(',');
             ObjectReader oReader = csvMapper.reader(Product.class).with(schema);
 
-            try (Reader reader = new FileReader(file)) {
                 MappingIterator<Product> mi = oReader.readValues(reader);
                 while (mi.hasNext()) {
                     Product current = mi.next();
                     products.put(current.getProductId(), current);
                 }
-            }
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -51,3 +56,6 @@ public class ProductService {
         return products;
     }
 }
+
+
+
